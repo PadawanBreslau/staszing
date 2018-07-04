64c4bbdc91547a2378382f9a8c9a79523a8b522c
Staszek Zawadzki <s.zawadzki@visuality.pl>
2018-07-04 05:03:45 +0200

    Add gitlab

--- Gemfile
+++ Gemfile
@@ -1,5 +1,6 @@
 source 'https://rubygems.org'
 
 gem 'activesupport', '4.1.4'
+gem 'actionview'
 gem 'smashing'
 gem 'pry'
---
+++ jobs/gitlab.rb
@@ -0,0 +1,29 @@
+#!/usr/bin/env ruby
+require 'net/http'
+require 'json'
+require 'action_view'
+
+include ActionView::Helpers::DateHelper
+
+gitlab_token = "c_ym3dVsG-sG6zRsxJXy"
+gitlab_uri = "https://gitlab.com/"
+
+SCHEDULER.every '30m', :first_in => 0 do |job|
+  uri = URI("#{gitlab_uri}api/v4/projects/3938540/events?private_token=#{gitlab_token}")
+  response = Net::HTTP.get(uri)
+  events = JSON.parse(response)
+
+  last_push = events.select{|e| e["action_name"] == 'pushed to'}.first["created_at"]
+  last_push_h = distance_of_time_in_words_to_now(Time.parse(last_push))
+
+  uri_commit = URI("#{gitlab_uri}api/v4/projects/3938540/repository/commits?private_token=#{gitlab_token}")
+  response = Net::HTTP.get(uri_commit)
+  commit = JSON.parse(response).first
+
+
+  last_commit_time =  commit["created_at"]
+  last_commit_time_h = distance_of_time_in_words_to_now(Time.parse(last_commit_time))
+  last_commit_msg = commit["message"]
+
+  send_event('gitlab', { items: [{ label: 'Last push', value: last_push_h},  { label: 'Last commit', value: last_commit_time_h}, { label: 'Commit', value: last_commit_msg} ] })
+end # SCHEDULER
