57cdc776d47efc83610362f76f50c759166ccb8a
Staszek Zawadzki <s.zawadzki@visuality.pl>
2018-07-16 17:17:53 +0200

    Waka client fix

--- jobs/wakatime.rb
+++ jobs/wakatime.rb
@@ -6,23 +6,21 @@ SCHEDULER.every '12m', :first_in => 0 do |job|
       api_key: ENV['WAKA_KEY']
   })
 
+  @waka_client = Wakatime::Client.new(@session)
+
   durations = []
-  @client = Wakatime::Client.new(@session)
+  projects = Hash.new(0)
 
   0.upto(6) do |i|
-    durations.concat @client.durations(Date.today - i)
+    durations.concat @waka_client.durations(Date.today - i)
   end
-
-  projects = Hash.new(0)
 
   durations.each do |duration|
     project = duration['project']
     time = duration['duration']
-
     projects[project] += time.round
   end
 
   projects.delete_if{|_, v| v < 300 }
-
   send_event('wakatime', {labels: projects.keys, datasets: projects.values.map{|v| (v/60).round} })
 end
