acaec316be79d2b9eba5a40670c3849b648704b6
Staszek Zawadzki <s.zawadzki@visuality.pl>
2018-08-13 13:29:27 +0200

    Fixes

--- jobs/sport_total.rb
+++ jobs/sport_total.rb
@@ -1,11 +1,8 @@
 #!/usr/bin/env ruby
 
-SCHEDULER.every '15m', :first_in => 0 do |job|
-  send_event('sport_total', { text: left_km.to_s + " km", color: sport_total_color })
-end
-
-def left_km
-  4800.0 - ($redis.get('ride_sum').to_f / 3.0 + $redis.get('run_sum').to_f)
+SCHEDULER.every '15m', :first_in => 1 do |job|
+  left_km = 4800.0 - ($redis.get('ride_sum').to_f / 3.0 + $redis.get('run_sum').to_f)
+  send_event('sport_total', { text: left_km.to_s + " km", color: sport_total_color(left_km) })
 end
 
 def days_till_b
@@ -15,7 +12,7 @@ def days_till_b
   days_till = b7s_date.mjd - today.mjd
 end
 
-def sport_total_color
+def sport_total_color(left_km)
   if left_km/days_till_b < 6.5
     green
   elsif left_km/days_till_b > 7.2
