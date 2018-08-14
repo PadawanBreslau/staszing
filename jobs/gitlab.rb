3ab5ee93b426dceddc4ce2fd570fa6050e2807cf
Staszek Zawadzki <s.zawadzki@visuality.pl>
2018-08-14 18:50:31 +0200

    Add Frontend ach support

--- jobs/gitlab.rb
+++ jobs/gitlab.rb
@@ -12,15 +12,28 @@ gitlab_uri = "https://gitlab.com/"
 SCHEDULER.every '25m', :first_in => 0 do |job|
   gitlab_token =  ENV['GITLAB_TOKEN']
   uri = URI("#{gitlab_uri}api/v4/projects/3938540/events?private_token=#{gitlab_token}")
+  uri_fr = URI("#{gitlab_uri}api/v4/projects/7940951/events?private_token=#{gitlab_token}")
+
   response = Net::HTTP.get(uri)
-  events = JSON.parse(response)
+  events_b = JSON.parse(response)
+
+  response_fr = Net::HTTP.get(uri_fr)
+  events_fr = JSON.parse(response)
+
+  events = events_b + events_fr
 
   last_push = events.select{|e| e["action_name"] == 'pushed to'}.first["created_at"]
   last_push_h = distance_of_time_in_words_to_now(Time.parse(last_push))
 
   uri_commit = URI("#{gitlab_uri}api/v4/projects/3938540/repository/commits?private_token=#{gitlab_token}")
   response = Net::HTTP.get(uri_commit)
-  commit = JSON.parse(response).first
+  commit_b = JSON.parse(response).first
+
+  uri_commit_fr = URI("#{gitlab_uri}api/v4/projects/7940951/repository/commits?private_token=#{gitlab_token}")
+  response = Net::HTTP.get(uri_commit_fr)
+  commit_fr = JSON.parse(response).first
+
+  commit = commit_b["created_at"] > commit_fr["created_at"] ? commit_b : commit_fr
 
   last_commit_time =  commit["created_at"]
   last_commit_time_h = distance_of_time_in_words_to_now(Time.parse(last_commit_time))
