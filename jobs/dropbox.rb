d850103a0fce6e0e70f26649ea60c8b4a7ddc0dd
Staszek Zawadzki <s.zawadzki@visuality.pl>
2018-08-17 12:44:29 +0200

    Dropbox fix

--- jobs/dropbox.rb
+++ jobs/dropbox.rb
@@ -5,28 +5,24 @@ SCHEDULER.every '24h', :first_in => 0 do |job|
   file_id = ENV['DROPBOX_FILE_ID']
   access_token = ENV['DROPBOX_TOKEN']
 
-  file_id = "9MhXzGfWtbQMdQZRRGpEa"
-  access_token = 'Bearer ddd2_uC7tZ4AAAAAAAADMM9GZPNJWagtT8st2YkZZUOjjmykeV6gkjIIdp2Q65k5'
-
   c = Curl::Easy.http_post("https://api.dropboxapi.com/2/paper/docs/download") do |curl|
     curl.headers["Authorization"] = access_token
-    curl.headers["Dropbox-API-Arg"] = {"doc_id": "9MhXzGfWtbQMdQZRRGpEa","export_format": "markdown"}.to_json
+    curl.headers["Dropbox-API-Arg"] = {"doc_id": file_id ,"export_format": "markdown"}.to_json
     curl.headers['Content-Type'] = ''
     curl.headers['Content-Length'] = ''
     curl.verbose = true
   end
 
-  prev_new_sentence_count = $redis.get("sentence_count").to_i
+  previous_sentence_count = $redis.get("sentence_count").to_i
+  current_sentence_count = c.body_str.split(".").count
 
   if $redis.get("last_check_date").to_i < Date.today.mjd
-    sentence_count = c.body_str.split(".").count
-
-    @count = sentence_count - prev_new_sentence_count
+    @count = current_sentence_count - previous_sentence_count
 
     $redis.set("last_check_date", Date.today.mjd)
-    $redis.set("sentence_count", sentence_count)
+    $redis.set("sentence_count", current_sentence_count)
   else
-    @count = prev_new_sentence_count
+    @count = previous_sentence_count
   end
 
   send_event('ksiazka_counter', current: @count)
